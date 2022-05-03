b619b4398c58a8f57cbc598cd379c76e031abb26
williamng0079 <williamng0079@gmail.com>
2022-05-03 23:49:55 +0100

    commented out

--- temp-test-scripts/wipStringValidate.js
+++ temp-test-scripts/wipStringValidate.js
@@ -1,11 +1,11 @@
-function inputValidation(string s) private pure returns (uint result, bool isTrue)
-    {
-        isTrue = false;
-        bytes memory b = bytes(s);
-        result = 0;
-        uint i;
-        for(i = 0; i < b.length; i++)
-        {
+//function inputValidation(string s) private pure returns (uint result, bool isTrue)
+    //{
+        //isTrue = false;
+        //bytes memory b = bytes(s);
+        //result = 0;
+        //uint i;
+        //for(i = 0; i < b.length; i++)
+        //{
             
-        }
-    }
+        //}
+    //}
