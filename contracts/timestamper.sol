6e8252a181c2c92a76a9362411a34a34d541ca91
williamng0079 <williamng0079@gmail.com>
2022-05-04 20:39:39 +0100

    added batch timestamp funtion in solidity

--- contracts/timestamper.sol
+++ contracts/timestamper.sol
@@ -2,11 +2,10 @@
 // The GPL license declared specifies the following: Everyone is permitted to copy and distribute verbatim copies of this license document, but changing it is not allowed
 // Source: https://spdx.org/licenses/GPL-3.0-or-later.html
 
-
 pragma solidity >=0.4.22 <0.9.0;            //This line specifies a range of solidity compilier versions that are accepted to compile this smart contract
 
 //import "hardhat/console.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";    //Ownable module allows ownership of this contract, only the specified owner can call the timestamp functions
+import "@openzeppelin/contracts/access/Ownable.sol";    
 //import "@openzeppelin/upgrades-core/contracts/Initializable.sol";   //For contract upgradeability
 
 //Only the address of the deployer can call functions tagged with onlyOwner within this smart contract.
@@ -16,28 +15,21 @@ contract Timestamper is Ownable
                                                                     //Private variable means that other accessing and modifying the information by other contracts are prevented. (Source: https://docs.soliditylang.org/en/v0.4.24/contracts.html)
     event Timestamp(string indexed hash);                           //Declareing an event allows arguments to be stored in the transaction's log, this is the main method of storing the user hash input onto the blockchain
                                                                     //The Argument "indexed" allows the respective arguments to be searched for (Source:https://docs.soliditylang.org/en/v0.4.24/contracts.html#events)
-    
     //Memory and calldata define the data area where a variable is stored. 
     //It is used to temporarily store variables and their values (Source:https://ethereum.stackexchange.com/questions/74442/when-should-i-use-calldata-and-when-should-i-use-memory)
     //calldata choosen due to lower gas usage.
     function timestamp(string calldata hash) public onlyOwner       
     {
         //_hash = hash;                                             //Commented out for gas efficiency
-
         //console.log("Timestamping:", hash);                       //This is commented out to improve gas efficientcy
-        
         emit Timestamp(hash);                                       //This allows the argument (supplied hash input) event indexed to be logged in the transaction's input field
     }
     
-    //The below function allows a call (zero gas usage) to display the most recent value stored in _hash 
-    //function retrieve() public view returns (string calldata)         
-    //{
-        //return _hash;
-    //}
-
-    //function batchTimestamp(string[] memory hashes) public onlyOwner {
-        //for (string i = 0; i < hashes.length; i++) {
-        //    emit Timestamp(hashes[i]);
-        //}
-    //}
+    function batchTimestamp(string[] calldata hash) public onlyOwner 
+    {
+        for (uint256 i = 0; i < hash.length; i++) 
+        {
+            emit Timestamp(hash[i]);
+        }
+    }
 }
